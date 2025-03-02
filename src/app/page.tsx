896d2975022ab7c389ba44c566f322fdd76a4add
abhi <buneeisslo@gmail.com>
2025-03-02 23:26:54 +0530

    Add: .cursor to ignore list and add besley font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,14 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-2xl text-rose-500">PDF</h1>
+      <h1 className="font-[besley] text-2xl text-rose-500">Review your CV</h1>
       <Button variant={"secondary"} size={"lg"}>
         LFG
       </Button>
+      <p className="font-[mono] text-lg">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique,
+        nulla.
+      </p>
     </div>
   );
 }
